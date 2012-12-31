b26c7f3366906386a32c82e4adf43f27f3ca0c9a
ronena <ronena@codeoasis.com>
2012-12-31 16:02:56 +0200

    merge fix

--- cc.sh
+++ cc.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-sudo php app/console cache:clear --env=prod --no-debug
-sudo php chmod 777 app/cache/ -R
-sudo php app/console assetic:dump --env=prod --no-debug
-=======
 php app/console cache:clear --env=prod --no-debug
 chmod 777 app/cache/ -R
 php app/console assetic:dump --env=prod --no-debug
->>>>>>> 3e7fd4a8bde78e68e2a5b307507af5ff9ec41e37
