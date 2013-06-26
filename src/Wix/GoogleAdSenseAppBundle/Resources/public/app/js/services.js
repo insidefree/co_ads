b5d63443f10e0a9b13b855305c5d76cff56314f8
ronena <ronena@codeoasis.com>
2013-06-26 14:55:03 +0300

    identations

--- src/Wix/GoogleAdSenseAppBundle/Resources/public/app/js/services.js
+++ src/Wix/GoogleAdSenseAppBundle/Resources/public/app/js/services.js
@@ -3,43 +3,45 @@
     'use strict';
 
     window.angular.module('adSenseApp.services', [])
-    /**
-     * allows access to query string parameters.
-     */
-    .factory('QueryParams', ['$location', function($location) {
-        var object = {},
-            params = $location.$$absUrl.slice($location.$$absUrl.indexOf('?') + 1).split('&'),
-            param;
+        /**
+         * allows access to query string parameters.
+         */
+        .factory('QueryParams', ['$location', function($location) {
+            var object = {},
+                params = $location.$$absUrl.slice($location.$$absUrl.indexOf('?') + 1).split('&'),
+                param;
 
-        for(param in params) {
-            object[params[param].split('=')[0]] = params[param].split('=')[1];
-        }
-
-        return object;
-    }])
-    /**
-     * wix sdk servers as an api to communicate with wix's editor.
-     */
-    .factory('WixSDK', ['$window', function($window) {
-        return $window.Wix;
-    }])
-    /**
-     * serves as a router to generate routes to a symfony2 backend. appends the instance for every request if it's available.
-     */
-    .factory('Router', ['$window', 'QueryParams', function($window, QueryParams) {
-        return {
-            path: function(name, params, absolute) {
-                params = params || {};
-
-                params.instance = QueryParams.instance || null;
-                params.compId = QueryParams.compId || null;
-                params.origCompId = QueryParams.origCompId || null;
-
-                return $window.Routing.generate(name, params, absolute);
-            },
-            url: function(name, params) {
-                return this.path(name, params, true);
+            for(param in params) {
+                object[params[param].split('=')[0]] = params[param].split('=')[1];
             }
-        };
-    }]);
+
+            return object;
+        }])
+
+        /**
+         * wix sdk servers as an api to communicate with wix's editor.
+         */
+        .factory('WixSDK', ['$window', function($window) {
+            return $window.Wix;
+        }])
+
+        /**
+         * serves as a router to generate routes to a symfony2 backend. appends the instance for every request if it's available.
+         */
+        .factory('Router', ['$window', 'QueryParams', function($window, QueryParams) {
+            return {
+                path: function(name, params, absolute) {
+                    params = params || {};
+
+                    params.instance = QueryParams.instance || null;
+                    params.compId = QueryParams.compId || null;
+                    params.origCompId = QueryParams.origCompId || null;
+
+                    return $window.Routing.generate(name, params, absolute);
+                },
+                url: function(name, params) {
+                    return this.path(name, params, true);
+                }
+            };
+        }]);
 }(window));
