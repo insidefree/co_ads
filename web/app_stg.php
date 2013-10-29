876354bcfbf915327da110722b16ea04d95ff0b6
eliorb <eliorb@codeoasis.com>
2013-10-29 16:32:48 +0200

    Corrected stg env

--- web/app_stg.php
+++ web/app_stg.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 //umask(0000);
 
 
-
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('stg', true);
