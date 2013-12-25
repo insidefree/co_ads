ddf9b369833c5723b9f965ae9eb0f1bae5230c42
eliorb <eliorb@codeoasis.com>
2013-12-25 14:40:40 +0200

    mobile view

---
+++ src/Wix/GoogleAdsenseBundle/Controller/MobileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Wix\GoogleAdsenseBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/mobile")
+ */
+class MobileController extends AppController
+{
+	/**
+	 * @Route("/", name="mobile", options={"expose"=true})
+	 * @Template()
+	 */
+	public function indexAction()
+	{
+		$params = array(
+			'adUnit' => $this
+					->getComponentDocument()
+					->getAdUnit(),
+			'mobile' => array(
+				"regular" => array("width" => 250, "height" => 250),
+				"tall" => array("width" => 120, "height" => 240),
+				"large" => array("width" => 320, "height" => 50)
+			)
+		);
+
+		$user = $this
+			->getUserDocument();
+
+		$component = $this
+			->getComponentDocument();
+
+		if ($component->hasAdUnit()) {
+			$params = array_merge($params, array(
+				'code' => $component->getAdCode(),
+				'domain' => $user->getDomain(),
+			));
+		}
+
+		return $params;
+	}
+}
