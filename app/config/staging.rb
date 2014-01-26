2099cae6cef65a4e67cdb24842ed826215f21f03
jonathan bensaid <john@bensaidj.com>
2014-01-26 17:54:05 +0200

    Separate the deploy in two servers: staging and production

---
+++ app/config/staging.rb
@@ -0,0 +1,48 @@
+set :application,  "wix-adsense"
+set :domain,       "wix.codeoasis.com"
+set :user,            "deploy"
+set :deploy_to,    "/var/www/wix-adsense"
+set :app_path,     "app"
+set :web_path,     "web"
+
+# set permissions
+set :writable_dirs,       ["app/cache", "app/logs"]
+set :webserver_user,      "www-data"
+set :permission_method,   :acl
+set :use_set_permissions, true
+set :use_sudo, false
+
+set :shared_files,    ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs"]
+
+set :repository,  "git@git.codeoasis.com:wix/wix-adsense.git"
+set :scm,         :git
+set :deploy_via,  :remote_cache
+
+# The following line tells Capifony to deploy the last Git tag.
+# Since Jenkins creates and pushes a tag following a successful build this should always be the last tested version of the code.
+set :branch, "jenkins-stage"
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set :use_composer,    true
+set :copy_vendors, true
+
+set  :keep_releases,  5
+
+# Confirmations will not be requested from the command line.
+set :interactive_mode, false
+
+task :restart_php do
+  run "cd #{release_path}; service php-fpm reload"
+
+  capifony_puts_ok
+end
+
+after "provide_permissions", "restart_php"
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
