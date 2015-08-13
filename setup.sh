948602c1309bc579618122afe621d8ec7393635c
Hagit Shmueli <hagitm@codeoasis.com>
2015-08-13 18:11:27 +0300

    chore(setup):setting write permissions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+echo "setting write permissions"
+
+sudo setfacl -R -m u:hagit:rwx -m u:www-data:rwx ./app/logs
+sudo setfacl -dR -m u:hagit:rwx -m u:www-data:rwx ./app/logs
+sudo setfacl -R -m u:hagit:rwx -m u:www-data:rwx ./app/cache
+sudo setfacl -dR -m u:hagit:rwx -m u:www-data:rwx ./app/cache
+
+
+composer install
+
 compass compile src/Wix/GoogleAdsenseBundle/Resources/public
 
 php app/console cache:clear -e=prod
