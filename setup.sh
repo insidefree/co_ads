f61b7fd05657ad747d3654ac6e439c53be4ef63c
Hagit Shmueli <hagitm@codeoasis.com>
2015-08-13 11:31:08 +0300

    chore(setup): added compass

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+compass compile src/Wix/GoogleAdsenseBundle/Resources/public
+
 php app/console cache:clear -e=prod
 php app/console fos:js-routing:dump -e=prod
 php app/console assetic:dump -e=prod
