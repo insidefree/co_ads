c7faf698a7b39552283edcf151895444d465d3cd
Hagit Shmueli <hagitm@codeoasis.com>
2015-11-08 11:55:36 +0200

    chore(): merged with hagit changes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+echo "setting write permissions"
+
+sudo setfacl -R -m u:`whoami`:rwx -m u:www-data:rwx ./app/logs
+sudo setfacl -dR -m u:`whoami`:rwx -m u:www-data:rwx ./app/logs
+sudo setfacl -R -m u:`whoami`:rwx -m u:www-data:rwx ./app/cache
+sudo setfacl -dR -m u:`whoami`:rwx -m u:www-data:rwx ./app/cache
+
+composer install
+
+compass compile src/Wix/GoogleAdsenseBundle/Resources/public
+
 php app/console cache:clear -e=prod
 php app/console fos:js-routing:dump -e=prod
 php app/console assetic:dump -e=prod
