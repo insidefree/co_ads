c61e5af1189cfaa5520fbaa3c28f2164827b311d
Hagit Shmueli <hagitm@codeoasis.com>
2015-11-10 16:52:39 +0200

    fix(): changes mongo script

--- testMongoScript.js
+++ testMongoScript.js
@@ -18,5 +18,11 @@ db.components.remove({"deletedAt": {$exists: false}, "pageId": { $exists: false}
 var result = db.components.find({"updatedDate": { $exists: true}, "deletedAt": {$exists: false}}, { "instanceId": 1, "pageId": 1});
 
 for each(var row in result){
-    db.components.remove({"instanceId": row.instanceId, "pageId": row.pageId, "updatedDate": { $exists: false}, "deletedAt": { $exists: false}});
+    db.components.remove({"instanceId": row.instanceId, "pageId": row.pageId, "adCode": { $exists: false},
+                          "updatedDate": { $exists: false}, "deletedAt": { $exists: false}});
 }
+
+var o0 = db.components.aggregate([{ $match: {"updatedDate": { $exists: true }, "deletedAt": {$exists: false} } }, { $group: { _id: { instanceId: '$instanceId', pageId: '$pageId' } } }, { $sort: { '_id.pageId': -1 } } ]);
+var o1 = o0.result.map(function(i){ return { instanceId: i._id.instanceId, pageId: i._id.pageId }; });
+db.components.remove({ "adCode": { $exists: false}, "updatedDate": { $exists: false}, "deletedAt": { $exists: false}, $or: o1 });
+
