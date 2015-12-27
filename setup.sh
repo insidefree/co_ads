3ca15bbe7f0848f417648f72426c500454dac0b4
Hagit Shmueli <hagitm@codeoasis.com>
2015-12-27 17:25:47 +0200

    chore(): fix setup file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-echo "setting write permissions"
-
 composer install
 
 compass compile src/Wix/GoogleAdsenseBundle/Resources/public
 
-php app/console cache:clear -e=prod
-php app/console fos:js-routing:dump -e=prod
-php app/console assetic:dump -e=prod
-php app/console assets:install -e=prod
+php app/console cache:clear -e=$l
+php app/console fos:js-routing:dump -e=$l
+php app/console assetic:dump -e=$l
+php app/console assets:install -e=$l
 
