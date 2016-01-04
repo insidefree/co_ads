fabb32d885d93c1b2eb308298330e0e382b07e87
Hagit Shmueli <hagitm@codeoasis.com>
2016-01-04 07:54:18 +0200

    chore(): change setup.sh file

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ composer install
 
 compass compile src/Wix/GoogleAdsenseBundle/Resources/public
 
-php app/console cache:clear -e=$l
-php app/console fos:js-routing:dump -e=$l
-php app/console assetic:dump -e=$l
-php app/console assets:install -e=$l
+php app/console cache:clear -e=prod
+php app/console fos:js-routing:dump -e=prod
+php app/console assetic:dump -e=prod
+php app/console assets:install -e=prod
 
