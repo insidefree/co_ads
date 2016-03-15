eaf656469210553856806a1db1ae842ab66712a4
Hagit Shmueli <hagitm@codeoasis.com>
2016-03-15 17:14:41 +0200

    cr(): changed code review

--- src/Wix/GoogleAdsenseBundle/Resources/public/js/widget/wixService.js
+++ src/Wix/GoogleAdsenseBundle/Resources/public/js/widget/wixService.js
@@ -157,7 +157,6 @@
         //==============================
 
         return {
-            init                            : init,
             getComponentInfo                : getComponentInfo,
             getCurrentPageId                : getCurrentPageId,
             getComponentInfoWithAppPageId   : getComponentInfoWithAppPageId,
