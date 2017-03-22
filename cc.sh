f536746a261a02d3eb8642c5c040565f02d98f64
Snir Segal <snir.segal@gmail.com>
2017-03-22 16:33:08 +0200

    fix cc

--- cc.sh
+++ cc.sh
@@ -1,3 +1,7 @@
-php app/console cache:clear --env=$1 --no-debug
-chmod 777 app/cache/ -R
-php app/console assetic:dump --env=$1 --no-debug
+#php app/console cache:clear --env=$1 --no-debug
+#chmod 777 app/cache/ -R
+#php app/console assetic:dump --env=$1 --no-debug
+
+app/console cache:clear -e=$1
+app/console assets:install -e=$1
+app/console assetic:dump -e=$1
